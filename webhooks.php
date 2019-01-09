01ad6a1b511523c06a7ecf6c208db52da26e6b61
signsurachet <s_teerapoj@hotmail.com>
2019-01-09 09:19:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'EG7nDLkTs5ZkOGlbJgG9CUhfnO3eiDzXVWaQQx8BPzj5kVGCoqK4rDkcNNNv1dCDCHKxRpxjwioaR7k65ekSHWWQcQT0Y5TbUp7uZaMeWd0/sPOoJDpnjDs8IOhMNNPSCGlt53PShHPXPU+IojgQhQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
